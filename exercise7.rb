61a7aebc62467cf3d4f31a777bcc15fbc2207fb9
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-25 16:44:30 -0400

    Printed the cohort names using the keys method.

--- exercise7.rb
+++ exercise7.rb
@@ -1,11 +1,17 @@
+## Step One
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+## Added a new cohort and population
 students[:cohort4] = 43
 
+## printing all cohorts and their populations to the console
 students.each do |cohort, students|
 	puts "#{cohort}: #{students} students"
 end
+
+## using the keys method
+puts students.keys
