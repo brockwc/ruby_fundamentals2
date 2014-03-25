d482bdc4bed11f7c81f9f0ae612225f2dcde583e
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-25 14:26:19 -0400

    User input in Fahrenheit is successfully converted to Celsius

--- exercise5.rb
+++ exercise5.rb
@@ -1,1 +1,9 @@
 puts "Input a temperature in Fahrenheit to be converted to Celsius:"
+
+user_input = gets.chomp.to_f
+
+def temp_calc(temp_in_f)
+	(temp_in_f - 32.0) * (5.0 / 9.0)
+end
+
+puts "The temperature in superior Celsius is #{temp_calc(user_input)}"
