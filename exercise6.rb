8a9b00dcc9f07cfa68f4f973bcadcc289a4ef292
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-25 16:30:02 -0400

    removed Cat Food from the list and printed the new list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,6 @@
+#Exercise 6
+
+##   Step One and Two
 grocery_list = ["Spam", "Cheez-Whiz", "Nutella", "Cat Food"]
 
 grocery_list << "Rice"
@@ -6,8 +9,27 @@ grocery_list.each do |item|
 	puts "* " + item
 end
 
+##   Step Three
 msg = "There are #{grocery_list.length} items in your grocery list. "
 
+##   Step Four
 if grocery_list.include?("Bananas") then puts "#{msg} Also, you need to buy Bananas too."
-else puts "#{msg}Also, you don't need no 'nanners."
-end 
+	else puts "#{msg}Also, you don't need no Bananas."
+end
+
+##   Step Five
+puts grocery_list[1]
+
+##   Step Six
+grocery_list.sort.each do |item|
+	puts "* " + item
+end
+
+##   Step Seven
+grocery_list.delete("Cat Food")
+
+puts "Here's your new list without Cat Food:"
+
+grocery_list.each do |item|
+	puts "* " + item
+end
